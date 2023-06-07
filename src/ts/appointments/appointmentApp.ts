1f0736622600e45df0bb78dfba6d0b105f5f0707
DevRenato <u18215194@gmail.com>
2023-06-07 17:51:55 -0500

    actualizando a nivel de archivos en ts

---
+++ src/ts/appointments/appointmentApp.ts
@@ -0,0 +1,13 @@
+const containerServices = document.querySelector('#services') as HTMLDivElement;
+const afterPage = document.querySelector('#toContinue') as HTMLButtonElement;
+const beforePage = document.querySelector('#toGoBack') as HTMLButtonElement;
+const userId = document.querySelector('#userId') as HTMLInputElement;
+const userName = document.querySelector('#name') as HTMLInputElement;
+const userLastname = document.querySelector('#lastname') as HTMLInputElement;
+const inputAppointmenDate = document.querySelector('#date') as HTMLInputElement;
+const inputAppointmenTime = document.querySelector('#time') as HTMLInputElement;
+
+const containerResumen = document.querySelector('#contentResumen') as HTMLDivElement;
+const containerTabs = document.querySelectorAll('#container-tabs button') as NodeListOf<HTMLDivElement>;
+
+export { };
---
+++ src/ts/store/appointmentStore.ts
@@ -0,0 +1,17 @@
+import { IServices } from "../interfaces"
+
+interface IStore {
+  services: IServices[],
+}
+
+const appointmentSatate = {
+  services: [],
+}
+
+const loadAppointments = () => {
+
+}
+
+export default {
+
+}
